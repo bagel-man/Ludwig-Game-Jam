7d3547202455adb572aa9e87f36d5a1a5229f728
bagel-man <tom@blowmage.com>
2023-02-16 20:08:52 -0700

    made the player controller better
    added a place holder model too. I still need to add wallrunning, mounting, and coyote time

--- LudwigGameJamGame/Assets/ThirdPersonController.cs
+++ LudwigGameJamGame/Assets/ThirdPersonController.cs
@@ -1,11 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ThirdPersonController : MonoBehaviour
 {
-    public CharacterController controller;
-    
+        public bool enableSprint = true;
+    public bool unlimitedSprint = false;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public float sprintSpeed = 7f;
+    public float sprintDuration = 5f;
+    public float sprintCooldown = .5f;
+    public Transform raycastStart;
+    public float raycastLength;
+
+    // Sprint Bar
+    public bool useSprintBar = true;
+    public bool hideBarWhenFull = true;
+
+    public float sprintBarWidthPercent = .3f;
+    public float sprintBarHeightPercent = .015f;
+  
+            private float sprintRemaining;
+                public Rigidbody rb;
+    private bool isSprintCooldown = false;
+    private float sprintCooldownReset;
+        public Image sprintBarBG;
+    public Image sprintBar;
+    private bool isSprinting = false;
+       public bool enableJump = true;
+    public KeyCode jumpKey = KeyCode.Space;
+    public float jumpPower = 5f;
+        private bool isGrounded = false;
+            public bool playerCanMove = true;
+    public float walkSpeed = 5f;
+    public float maxVelocityChange = 10f;
+
+    // Internal Variables
+    private bool isWalking = false;
     public Transform cam;
 
     public float speed = 6f;
@@ -13,16 +45,141 @@ public class ThirdPersonController : MonoBehaviour
     public float turnSmoothTime = 0.1f;
 
     float turnSmoothVelocity;
+    public bool lockCursor;
+        public bool crosshair = true;
+    public Sprite crosshairImage;
+    public Color crosshairColor = Color.white;
+        public Image crosshairObject;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        sprintCooldownReset = sprintCooldown;
+                if(lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
 
+   
+        else
+        {
+            crosshairObject.gameObject.SetActive(false);
+        }
+    }
+    void Update(){
+                if(enableSprint)
+        {
+            if(isSprinting)
+            {
+
+
+
+                // Drain sprint remaining while sprinting
+                if(!unlimitedSprint)
+                {
+                    sprintRemaining -= 1 * Time.deltaTime;
+                    if (sprintRemaining <= 0)
+                    {
+                        isSprinting = false;
+                        isSprintCooldown = true;
+                    }
+                }
+            }
+            else
+            {
+                // Regain sprint while not sprinting
+                sprintRemaining = Mathf.Clamp(sprintRemaining += 1 * Time.deltaTime, 0, sprintDuration);
+            }
+
+            // Handles sprint cooldown 
+            // When sprint remaining == 0 stops sprint ability until hitting cooldown
+            if(isSprintCooldown)
+            {
+                sprintCooldown -= 1 * Time.deltaTime;
+                if (sprintCooldown <= 0)
+                {
+                    isSprintCooldown = false;
+                }
+            }
+            else
+            {
+                sprintCooldown = sprintCooldownReset;
+            }
+
+            // Handles sprintBar 
+            if(useSprintBar && !unlimitedSprint)
+            {
+                float sprintRemainingPercent = sprintRemaining / sprintDuration;
+                sprintBar.transform.localScale = new Vector3(sprintRemainingPercent, 1f, 1f);
+            }
+        }
+        CheckGround();
+              if(enableJump && Input.GetKeyDown(jumpKey) && isGrounded)
+        {
+            Jump();
+        }
+    }
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+         // Calculate how fast we should be moving
+            Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+            // Checks if player is walking and isGrounded
+            // Will allow head bob
+            if (targetVelocity.x != 0 || targetVelocity.z != 0 && isGrounded)
+            {
+                isWalking = true;
+
+            }
+            else
+            {
+                isWalking = false;
+
+
+            }
+
+            // All movement calculations shile sprint is active
+            if (enableSprint && Input.GetKey(sprintKey) && sprintRemaining > 0f && !isSprintCooldown)
+            {
+                targetVelocity = transform.TransformDirection(targetVelocity) * sprintSpeed;
+
+                // Apply a force that attempts to reach our target velocity
+                Vector3 velocity = rb.velocity;
+                Vector3 velocityChange = (targetVelocity - velocity);
+                velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+                velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+                velocityChange.y = 0;
+
+                // Player is only moving when valocity change != 0
+                // Makes sure fov change only happens during movement
+                if (velocityChange.x != 0 || velocityChange.z != 0)
+                {
+                    isSprinting = true;
+
+  
+
+                }
+
+                rb.AddForce(velocityChange, ForceMode.VelocityChange);
+            }
+            // All movement calculations while walking
+            else
+            {
+                isSprinting = false;
+
+                targetVelocity = transform.TransformDirection(targetVelocity) * walkSpeed;
+
+                // Apply a force that attempts to reach our target velocity
+                Vector3 velocity = rb.velocity;
+                Vector3 velocityChange = (targetVelocity - velocity);
+                velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+                velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+                velocityChange.y = 0;
+
+                rb.AddForce(velocityChange, ForceMode.VelocityChange);
+            }
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -33,9 +190,39 @@ public class ThirdPersonController : MonoBehaviour
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
         }
 
+    }
+
+     private void CheckGround()
+    {
+    
+        Vector3 direction = transform.TransformDirection(Vector3.down);
+   
+        if (Physics.Raycast(raycastStart.position, direction, out RaycastHit hit, raycastLength))
+        {
+            Debug.DrawRay(raycastStart.position, direction * raycastLength, Color.red);
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+        private void Jump()
+    {
+        Debug.Log("sus");
+        // Adds force to the player rigidbody to jump
+        if (isGrounded)
+        {
+            rb.AddForce(0f, jumpPower, 0f, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+        // When crouched and using toggle system, will uncrouch for a jump
+
+     
     }
 }
